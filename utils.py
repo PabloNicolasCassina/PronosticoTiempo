2aa864e04290144f86ef3abc358f5b4ac27fd9b4
Nico Cassina <cassinanico@gmail.com>
2023-05-19 01:21:55 -0300

    Agregando req y utils.py

---
+++ utils.py
@@ -0,0 +1,71 @@
+
+import pandas as pd
+from twilio.rest import Client
+from datetime import datetime
+import requests
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+
+TWILIO_ACCOUNT_SID ="AC2d5ddb7cb6bc54ecaf3910ecffbc03e0"
+TWILIO_AUTH_TOKEN ="c41de20b4b6c255f65859f0a4d5a3147"
+PHONE_NUMBER ="+12545705477"
+API_KEY_WAPI = '8ab276a6f3f24bc8987184908231805'
+
+def get_date():
+
+    input_date = datetime.now()
+    input_date = input_date.strftime("%Y-%m-%d")
+
+    return input_date
+
+def request_wapi(api_key,query):
+
+    url_clima = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+query+'&days=1&aqi=no&alerts=no'
+
+    try :
+        response = requests.get(url_clima).json()
+    except Exception as e:
+        print(e)
+
+    return response
+
+def get_forecast(response,i):
+
+    fecha = response['forecast']['forecastday'][0]['hour'][i]['time'].split()[0]
+    hora = int(response['forecast']['forecastday'][0]['hour'][i]['time'].split()[1].split(':')[0])
+    condicion = response['forecast']['forecastday'][0]['hour'][i]['condition']['text']
+    tempe = response['forecast']['forecastday'][0]['hour'][i]['temp_c']
+    rain = response['forecast']['forecastday'][0]['hour'][i]['will_it_rain']
+    prob_rain = response['forecast']['forecastday'][0]['hour'][i]['chance_of_rain']
+
+    return fecha,hora,condicion,tempe,rain,prob_rain
+
+def create_df(data):
+
+    col = ['Fecha','Hora','Condicion','Temperatura','Lluvia','prob_lluvia']
+    df = pd.DataFrame(data,columns=col)
+    df = df.sort_values(by = 'Hora',ascending = True)
+
+    df_rain = df[(df['Lluvia']==1) & (df['Hora']>6) & (df['Hora']< 22)]
+    df_rain = df_rain[['Hora','Condicion']]
+    df_rain.set_index('Hora', inplace = True)
+
+    return df_rain
+
+def send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,df,query):
+
+    account_sid = TWILIO_ACCOUNT_SID
+    auth_token = TWILIO_AUTH_TOKEN
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+                    .create(
+                        body='\nHola! \n\n\n El pronostico de lluvia hoy '+ input_date +' en ' + query +' es : \n\n\n ' + str(df),
+                        from_=PHONE_NUMBER,
+                        to='+543512334798'
+                    )
+
+    return message.sid
